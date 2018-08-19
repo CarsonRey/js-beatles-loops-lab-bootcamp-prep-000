211b727e1c90ed2c95169c1015daedd21c79090d
Carson Rey <c.rey494@gmail.com>
2018-08-19 04:31:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function iLoveTheBeatles(n) {
   do {
     array.push("I love the Beatles!");
   }
-  while (incrementLoop() && array.length < 15);
+  while (incrementLoop() && n < 15);
   return array;
 }
 
