1a3007a7576c3f654fa98fdfc118c006020ca5b1
Carson Rey <c.rey494@gmail.com>
2018-08-19 03:55:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   var otherArray = [];
   var i = 0;
-  while (i < 4) {
+  while (otherArray.length < 4) {
     console.log(otherArray.push(`${facts[i]}!!!`))
     i++;
   }
